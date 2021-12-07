7c0446e4c2279f9e3a7688a40b5dfac9ff81a1e8
unknown <julicarra@hotmail.com>
2021-12-07 13:35:46 -0300

    add and remove books

--- src/components/BookLog.js
+++ src/components/BookLog.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const BookLog = () => (
-  <div>
-    <h3 className="book-title">Book Title</h3>
-    <button type="button">Remove</button>
-  </div>
-);
+const BookLog = (props) => {
+  const { title, category, id } = props;
+  const dispatch = useDispatch();
+
+  const handleRemove = (e) => {
+    e.stopPropagation();
+    dispatch(removeBook(id));
+  };
+  return (
+    <div>
+      <h3 className="book-title">{title}</h3>
+      <p>{category}</p>
+      <button type="button" onClick={(e) => handleRemove(e)}>Remove</button>
+    </div>
+  );
+};
+
+BookLog.propTypes = {
+  title: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default BookLog;
